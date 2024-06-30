9a69392409b4dc9a796e4ed940aaa8bb11b662d9
Cléo "Chii Kozume" Lima <cleolimadev@gmail.com>
2024-06-30 13:10:16 -0400

    fix: ajuste do swagger, agora funcionando

--- schemas/usuario.py
+++ schemas/usuario.py
@@ -1,8 +1,6 @@
 from pydantic import BaseModel, Field, ConfigDict
 
 class UsuarioSchema(BaseModel):
-    """ Representação de um novo usuário.
-    """
     username: str = Field(..., max_length=80)
     password: str = Field(..., min_length=6)
 
